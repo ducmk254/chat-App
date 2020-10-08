976e08f3ceaa85606651435a9deed6f7492cc848
Minh Duc <ducmk254@gmail.com>
2020-10-08 22:02:00 +0700

    initial chat-app

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express'),
+      app = express(),
+      path = require('path'),
+      pathPublic = path.join(__dirname,"../public"),
+      port = process.env.PORT || 3000
+
+app.use(express.static(pathPublic))
+
+
+
+app.listen(port,()=>{
+    console.log(`Server is up on port ${port}`)
+})
