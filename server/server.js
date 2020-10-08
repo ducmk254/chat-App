4bf0ce69cd451dddfc561f99f2b5c2b680d9e8e0
Minh Duc <ducmk254@gmail.com>
2020-10-08 23:30:07 +0700

    done for io and socket

--- server/server.js
+++ server/server.js
@@ -1,9 +1,29 @@
-const express = require('express'),
-      app = express(),
-      path = require('path'),
-      pathPublic = path.join(__dirname,"../public"),
-      port = process.env.PORT || 3000;
+const express = require('express')
+let  app = express()
+let   path = require('path')
+let    pathPublic = path.join(__dirname,"../public")
+let      port = process.env.PORT || 3000
+      
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+
 app.use(express.static(pathPublic));
-app.listen(port,()=>{
+
+
+app.get('/',(req,res)=>{
+    res.sendFile('index.html');
+})
+
+io.on('connection',(socket)=>{
+    console.log('A new user just connected');
+
+    socket.on('disconnect',()=>{
+        console.log('User disconnected')
+    })
+    
+})
+
+
+http.listen(port,()=>{
     console.log(`Server is up on port ${port}`);
 })
